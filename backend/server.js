e3e80f1365477efe4752253592a4ac57f8e3110e
Shashank vaish <shashankvaish72@gmail.com>
2026-04-09 15:21:56 +0530

    Fix cors

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({origin: "*"}));
+app.use(cors({origin: "https://raja-fashion.vercel.app", credentials: true}));
 app.use("/uploads", express.static("uploads"));
 
 mongoose.connect(process.env.MONGO_URI)
